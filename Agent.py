e98027b97f3606dfb6633762a76ecec1523b28b7
Alexander Baumann <88405797+alexbaumi@users.noreply.github.com>
2021-11-13 22:37:15 +0100

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -62,8 +62,8 @@ class Agent(object):
 
         # Define algorithm variables
         self.clip=opt.clip
-        self.ppo=opt.ppo
-        self.dl=opt.dl
+        self.ppo=opt.PPO
+        self.kl=opt.KL
         
         #Define hyperparameters
         self.K=opt.K_epochs
@@ -128,9 +128,7 @@ class Agent(object):
 
             adv=adv*self.discount*self.gae_lambda*(1-self.dones[t])+delta
             advantage[t]=adv
-
-        
-                
+              
         rewards = torch.FloatTensor(rewards).to(self.device)
         advantage = torch.FloatTensor(advantage).to(self.device)
         #Normalize the advantage
@@ -139,13 +137,8 @@ class Agent(object):
         old_states = torch.squeeze(torch.stack(self.states, dim=0)).detach().to(self.device)
         old_actions = torch.squeeze(torch.stack(self.actions, dim=0)).detach().to(self.device)
         old_logprobs = torch.squeeze(torch.stack(self.log_probs, dim=0)).detach().to(self.device)
-        
-        
-        pi_old=self.actor(old_states).view((-1,self.action_space.n))
-
-        #state_values=self.critic(old_states).view((-1,self.action_space.n))        
-        #state_value=state_values.gather(1,old_actions.view((-1,1)))
-        
+                
+        pi_old=self.actor(old_states).view((-1,self.action_space.n))          
         state_value=self.critic(old_states).view(-1) 
         
         for _ in range(self.K):
@@ -208,9 +201,7 @@ class Agent(object):
                 #td_error=self.rewards[t]+self.discount*(1-self.dones[t])*self.values[t+1]-self.values[t]
                 
             advantage[t]=rewards[t]-self.values[t]
-
-        
-                
+          
         rewards = torch.FloatTensor(rewards).to(self.device)
         advantage = torch.FloatTensor(advantage).to(self.device)
         #Normalize the advantage
@@ -221,10 +212,6 @@ class Agent(object):
         old_logprobs = torch.squeeze(torch.stack(self.log_probs, dim=0)).detach().to(self.device)
         
         pi_old=self.actor(old_states).view((-1,self.action_space.n))
-
-        #state_values=self.critic(old_states).view((-1,self.action_space.n))        
-        #state_value=state_values.gather(1,old_actions.view((-1,1)))
-        
         state_value=self.critic(old_states).view(-1) 
         
         for _ in range(self.K):
@@ -293,8 +280,7 @@ class Agent(object):
         old_logprobs = torch.squeeze(torch.stack(self.log_probs, dim=0)).detach().to(self.device)
 
         state_values=self.critic(old_states).view(-1)       
-        
-        
+       
         for _ in range(self.K):
 
             probs = self.actor(old_states)
@@ -336,7 +322,7 @@ class Agent(object):
 
         if self.clip:
             self.learn_clip()
-        elif self.dl:
+        elif self.kl:
             self.learn_kl()
         elif self.ppo:
             self.learn_ppo()
